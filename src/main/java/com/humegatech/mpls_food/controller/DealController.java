537ca89318795b4eba21ccc9ab28a76ec21996d9
eebbesen <eebbesen.git@gmail.com>
2022-07-27 18:10:38 -0500

    clean up imports

--- src/main/java/com/humegatech/mpls_food/controller/DealController.java
+++ src/main/java/com/humegatech/mpls_food/controller/DealController.java
@@ -5,17 +5,14 @@ import com.humegatech.mpls_food.model.DealDTO;
 import com.humegatech.mpls_food.repos.PlaceRepository;
 import com.humegatech.mpls_food.service.DealService;
 import com.humegatech.mpls_food.util.WebUtils;
-import java.util.stream.Collectors;
-import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 
 @Controller
