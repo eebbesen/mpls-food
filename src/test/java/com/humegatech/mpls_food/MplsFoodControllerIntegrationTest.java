18eed096d23a0cb5f0c53ff293a3f118d79eca77
eebbesen <eebbesen.git@gmail.com>
2022-07-27 15:27:27 -0500

    adding some tests

---
+++ src/test/java/com/humegatech/mpls_food/MplsFoodControllerIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.humegatech.mpls_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MplsFoodControllerIntegrationTest {
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        assertThat(response.getBody()).contains("Downtown deals for you!");
+    }
+}
