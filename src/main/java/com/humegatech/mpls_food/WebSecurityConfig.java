b3028b710565ffe85d4bc19254ee06a28ef2fb94
eebbesen <eebbesen.git@gmail.com>
2022-07-31 16:24:38 -0500

    Toggling what's available to view based on role; adding login/logout

--- src/main/java/com/humegatech/mpls_food/WebSecurityConfig.java
+++ src/main/java/com/humegatech/mpls_food/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
                 .and()
