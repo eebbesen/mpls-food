bb631339370bf34ff8b799a26457bdad0f4351dc
eebbesen <eebbesen.git@gmail.com>
2022-08-29 09:01:15 -0500

    adding static keys to inserts

--- src/test/test_data/deals_202208101900.sql
+++ src/test/test_data/deals_202208101900.sql
@@ -1,35 +1,35 @@
-INSERT INTO public.deals (date_created,description,last_updated,place_id) VALUES
-	 ('2022-07-27 13:15:04.809267','$2.50 slices from 10:30 - 11:30','2022-07-27 13:15:04.809267',10000),
-	 ('2022-07-27 23:40:06.41481','$8.25 plain burger ($2.30 off).
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id) VALUES
+	 (50,'2022-07-27 13:15:04.809267','$2.50 slices from 10:30 - 11:30','2022-07-27 13:15:04.809267',10000),
+	 (51,'2022-07-27 23:40:06.41481','$8.25 plain burger ($2.30 off).
 
 Discount does not appear to be applied with online ordering.','2022-07-27 23:40:06.41481',10004),
-	 ('2022-07-27 23:41:12.97514','$8.55 cheeseburger ($2.30 off).
+	 (52,'2022-07-27 23:41:12.97514','$8.55 cheeseburger ($2.30 off).
 
 Discount does not appear to be applied with online ordering.','2022-07-27 23:41:12.97514',10004),
-	 ('2022-07-27 23:42:17.849534','$8.95 cajun cheeseburger ($2.30 off).
+	 (53,'2022-07-27 23:42:17.849534','$8.95 cajun cheeseburger ($2.30 off).
 
 Discount does not appear to be applied with online ordering.','2022-07-27 23:42:17.849534',10004),
-	 ('2022-07-27 23:45:21.760957','$10.25 bacon cheeseburger ($2.30 off).
+	 (54,'2022-07-27 23:45:21.760957','$10.25 bacon cheeseburger ($2.30 off).
 
 Discount does not appear to be applied with online ordering.','2022-07-27 23:45:21.760957',10004),
-	 ('2022-07-27 23:46:01.33711','$9.95 mushroom swiss burger ($2.30 off).
+	 (55,'2022-07-27 23:46:01.33711','$9.95 mushroom swiss burger ($2.30 off).
 
 Discount does not appear to be applied with online ordering.','2022-07-27 23:46:01.33711',10004),
-	 ('2022-07-27 23:46:38.15725','$9.95 Minnesota burger ($2.30 off).
+	 (56,'2022-07-27 23:46:38.15725','$9.95 Minnesota burger ($2.30 off).
 
 Discount does not appear to be applied with online ordering.','2022-07-27 23:46:38.15725',10004),
-	 ('2022-07-27 23:56:35.586779','Buy one hot dog get another half off','2022-07-27 23:56:35.586779',10011),
-	 ('2022-07-27 23:16:36.873815','$1.10 crispy tacos (half price)','2022-07-28 00:10:04.276178',2),
-	 ('2022-07-27 23:59:01.748955','$4.50 for two Ball Park hot dogs','2022-07-28 00:10:22.400211',10011);
-INSERT INTO public.deals (date_created,description,last_updated,place_id) VALUES
-	 ('2022-07-27 23:59:20.578794','$5.50 for two Ball Park chili dogs','2022-07-28 00:10:37.151898',10011),
-	 ('2022-07-28 00:00:17.810187','$4.95 for a Vienna Beef hot dog with chips and a 24 ounce fountain drink.
+	 (57,'2022-07-27 23:56:35.586779','Buy one hot dog get another half off','2022-07-27 23:56:35.586779',10011),
+	 (58,'2022-07-27 23:16:36.873815','$1.10 crispy tacos (half price)','2022-07-28 00:10:04.276178',2),
+	 (59,'2022-07-27 23:59:01.748955','$4.50 for two Ball Park hot dogs','2022-07-28 00:10:22.400211',10011);
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id) VALUES
+	 (60,'2022-07-27 23:59:20.578794','$5.50 for two Ball Park chili dogs','2022-07-28 00:10:37.151898',10011),
+	 (61,'2022-07-28 00:00:17.810187','$4.95 for a Vienna Beef hot dog with chips and a 24 ounce fountain drink.
 $2.75 to add a beef hot dog.','2022-07-28 00:11:26.008014',10011),
-	 ('2022-07-28 00:01:23.103103','$4.55 for a Ball Park hot dog with chips and a 24 ounce fountain drink.
+	 (62,'2022-07-28 00:01:23.103103','$4.55 for a Ball Park hot dog with chips and a 24 ounce fountain drink.
 $2.25 to add a dog.','2022-07-28 00:12:30.24485',10011),
-	 ('2022-07-28 00:03:39.268623','$3.00 (tax included) for any flavor small malt or root beer float.','2022-07-28 00:13:10.389212',10011),
-	 ('2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.','2022-07-28 00:14:24.973642',10000),
-	 ('2022-07-27 23:16:57.658109','$1.00 off Taco Bravo','2022-08-10 17:06:09.120356',2),
-	 ('2022-08-10 17:11:39.566796','$1.00 off Meat and Potato Burrito ($5.21 tax included)','2022-08-10 17:23:31.285314',2),
-	 ('2022-08-10 17:23:55.716542','$1.00 off taco salad','2022-08-10 17:23:55.716542',2),
-	 ('2022-08-10 17:24:21.696795','$1.39 softshell taco','2022-08-10 17:24:21.696795',2);
+	 (63,'2022-07-28 00:03:39.268623','$3.00 (tax included) for any flavor small malt or root beer float.','2022-07-28 00:13:10.389212',10011),
+	 (64,'2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.','2022-07-28 00:14:24.973642',10000),
+	 (65,'2022-07-27 23:16:57.658109','$1.00 off Taco Bravo','2022-08-10 17:06:09.120356',2),
+	 (66,'2022-08-10 17:11:39.566796','$1.00 off Meat and Potato Burrito ($5.21 tax included)','2022-08-10 17:23:31.285314',2),
+	 (67,'2022-08-10 17:23:55.716542','$1.00 off taco salad','2022-08-10 17:23:55.716542',2),
+	 (68,'2022-08-10 17:24:21.696795','$1.39 softshell taco','2022-08-10 17:24:21.696795',2);
