c094b977d8e8f75d6590f90dbf6ccdcaf9a847c3
eebbesen <eebbesen.git@gmail.com>
2022-09-05 08:43:46 -0500

    more tests and now adding fixtures

--- src/test/java/com/humegatech/mpls_food/controllers/DealControllerTest.java
+++ src/test/java/com/humegatech/mpls_food/controllers/DealControllerTest.java
@@ -1,6 +1,12 @@
 package com.humegatech.mpls_food.controllers;
 
+import com.humegatech.mpls_food.TestObjects;
+import com.humegatech.mpls_food.domains.Deal;
+import com.humegatech.mpls_food.domains.Place;
+import com.humegatech.mpls_food.repositories.DealRepository;
+import com.humegatech.mpls_food.repositories.PlaceRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +18,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.util.NestedServletException;
 
+import javax.transaction.Transactional;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -19,12 +27,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class DealControllerTest {
     @Autowired
+    PlaceRepository placeRepository;
+    @Autowired
+    DealRepository dealRepository;
+    @Autowired
     private MockMvc mvc;
-
     @Autowired
     private WebApplicationContext webApplicationContext;
+    private Place place;
+    private Deal deal;
+
+    @BeforeEach
+    void setUp() {
+        place = placeRepository.save(TestObjects.ginellis());
+        Deal d = TestObjects.fridayTwofer();
+        d.setPlace(place);
+        deal = dealRepository.save(d);
+    }
 
     @Test
     void testList() throws Exception {
@@ -85,8 +107,7 @@ public class DealControllerTest {
                         .with(csrf())
                         .param("description", "Test deal")
                         .param("friday", "true")
-                        .param("place", "2")
-                        .param("place_id", "true"))
+                        .param("place", place.getId().toString()))
                 .andExpect(status().is3xxRedirection());
 
     }
@@ -98,23 +119,44 @@ public class DealControllerTest {
                         .with(csrf())
                         .param("description", "Test deal")
                         .param("friday", "true")
-                        .param("place", "2")
-                        .param("place_id", "true"))
+                        .param("place", place.getId().toString()))
                 .andExpect(status().is3xxRedirection());
-
     }
 
     // todo this test will fail once the app handles bad payloads better
+    // for now the expectation is never used
     @Test
     @WithMockUser(roles = "ADMIN")
-    void testPostAddAdminBadPayload() throws Exception {
-        Assertions.assertThrows(NestedServletException.class, () -> {
-            mvc.perform(MockMvcRequestBuilders.post("/deals/add")
-                            .with(csrf())
-                            .param("friday", "true")
-                            .param("place", "2")
-                            .param("place_id", "true"))
-                    .andExpect(status().is4xxClientError());
-        });
+    void testPostAddAdminBadPayload() {
+        Assertions.assertThrows(NestedServletException.class, () -> mvc.perform(MockMvcRequestBuilders.post("/deals/add")
+                        .with(csrf())
+                        .param("friday", "true")
+                        .param("place", place.getId().toString()))
+                .andExpect(status().is4xxClientError()));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void testGetEditAdmin() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(String.format("/deals/edit/%d", deal.getId())).accept(MediaType.APPLICATION_XML))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Edit Deal")));
+    }
+
+    @Test
+    @WithMockUser
+    void testGetEditUserNotAllowed() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(String.format("/deals/edit/%d", deal.getId())).accept(MediaType.APPLICATION_XML))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void testPostEditAdmin() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post(String.format("/deals/edit/%d", deal.getId()))
+                        .with(csrf())
+                        .param("description", "Test deal updated")
+                        .param("place", place.getId().toString()))
+                .andExpect(status().is3xxRedirection());
     }
 }
