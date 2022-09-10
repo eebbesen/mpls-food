cf74403ecb166582618f7820fe298d87f9827b79
eebbesen <eebbesen.git@gmail.com>
2022-09-10 11:13:05 -0500

    day of week filter

--- src/main/java/com/humegatech/mpls_food/controllers/DayController.java
+++ src/main/java/com/humegatech/mpls_food/controllers/DayController.java
@@ -5,11 +5,15 @@ import com.humegatech.mpls_food.util.WebUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.DayOfWeek;
 
 @Controller
 @RequestMapping("/days")
@@ -20,9 +24,34 @@ public class DayController {
         this.dayService = dayService;
     }
 
+    private static DayOfWeek handleDayOfWeekFilter(final String dayOfWeekString) {
+        if (ObjectUtils.isEmpty(dayOfWeekString)) {
+            return null;
+        }
+
+        DayOfWeek dayOfWeek = null;
+
+        try {
+            dayOfWeek = DayOfWeek.valueOf(dayOfWeekString);
+        } catch (IllegalArgumentException e) {
+//            todo log error
+//            result.addError(new ObjectError("globalError", String.format("Invalid day of the week: %s", dayOfWeekString)));
+            return null;
+        }
+
+        return dayOfWeek;
+    }
+
     @GetMapping
-    public String list(final Model model) {
-        model.addAttribute("days", dayService.findAll());
+    public String list(final Model model, final HttpServletRequest request) {
+        final String dayOfWeekFilter = request.getParameter("dayOfWeek");
+        final DayOfWeek dayOfWeek = handleDayOfWeekFilter(dayOfWeekFilter);
+
+        if (null != dayOfWeek) {
+            model.addAttribute("days", dayService.findByDayOfWeek(dayOfWeek));
+        } else {
+            model.addAttribute("days", dayService.findAll());
+        }
 
         return "days/list";
     }
@@ -34,6 +63,5 @@ public class DayController {
         redirectAttributes.addFlashAttribute(WebUtils.MSG_INFO, WebUtils.getMessage("day.delete.success"));
         return "redirect:/days";
     }
-
 
 }
--- src/test/java/com/humegatech/mpls_food/controllers/DayControllerTest.java
+++ src/test/java/com/humegatech/mpls_food/controllers/DayControllerTest.java
@@ -6,13 +6,17 @@ import com.humegatech.mpls_food.domains.Deal;
 import com.humegatech.mpls_food.domains.Place;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.DayOfWeek;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,6 +25,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class DayControllerTest extends MFControllerTest {
     private Place place;
     private Deal deal;
+
+    @Autowired
+    private DayController controller;
 
     @BeforeEach
     void setUp() {
@@ -33,6 +40,42 @@ public class DayControllerTest extends MFControllerTest {
     @Test
     void testList() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/days").accept(MediaType.APPLICATION_XML))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Login")))
+                .andExpect(content().string(containsString("slices")))
+                .andExpect(content().string(containsString("Ginelli&#39;s")));
+    }
+
+    @Test
+    void testListWithDayOfWeek() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/days?dayOfWeek=FRIDAY").accept(MediaType.APPLICATION_XML))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Login")))
+                .andExpect(content().string(containsString("slices")))
+                .andExpect(content().string(containsString("Ginelli&#39;s")));
+    }
+
+    @Test
+    void testListWithDayOfWeekNoRecords() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/days?dayOfWeek=MONDAY").accept(MediaType.APPLICATION_XML))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Login")))
+                .andExpect(content().string(not(containsString("slices"))))
+                .andExpect(content().string(not(containsString("Ginelli&#39;s"))));
+    }
+
+    @Test
+    void testListWithDayOfWeekNoDay() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/days?dayOfWeek=").accept(MediaType.APPLICATION_XML))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Login")))
+                .andExpect(content().string(containsString("slices")))
+                .andExpect(content().string(containsString("Ginelli&#39;s")));
+    }
+
+    @Test
+    void testListWithDayOfInvalidDay() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/days?dayOfWeek=BLAH").accept(MediaType.APPLICATION_XML))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Login")))
                 .andExpect(content().string(containsString("slices")))
@@ -78,5 +121,32 @@ public class DayControllerTest extends MFControllerTest {
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection());
         assertEquals(Optional.empty(), dayRepository.findById(day.getId()));
+    }
+
+    @Test
+    void testHandleDayOfWeek() throws Exception {
+        DayOfWeek dow = ReflectionTestUtils.invokeMethod(controller, "handleDayOfWeekFilter", "TUESDAY");
+        assertEquals(DayOfWeek.TUESDAY, dow);
+    }
+
+    @Test
+    void testHandleDayOfWeekNullDow() throws Exception {
+        final String nullString = null;
+        final DayOfWeek dow = ReflectionTestUtils.invokeMethod(controller, "handleDayOfWeekFilter", nullString);
+        assertEquals(null, dow);
+    }
+
+    @Test
+    void testHandleDayOfWeekEmptyDow() throws Exception {
+        final String emptyString = "";
+        final DayOfWeek dow = ReflectionTestUtils.invokeMethod(controller, "handleDayOfWeekFilter", emptyString);
+        assertEquals(null, dow);
+    }
+
+    @Test
+    void testHandleDayOfWeekInvalidDow() throws Exception {
+        final String invalid = "BLORTSDAY";
+        final DayOfWeek dow = ReflectionTestUtils.invokeMethod(controller, "handleDayOfWeekFilter", invalid);
+        assertEquals(null, dow);
     }
 }
