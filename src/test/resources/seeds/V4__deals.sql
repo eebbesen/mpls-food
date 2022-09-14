8adfbe35cc8731c64c3c28a2a4d9d2cd8fada378
eebbesen <eebbesen.git@gmail.com>
2022-09-14 14:37:20 -0500

    UI and data changes to support the new fields in edit mode

--- src/test/resources/seeds/V4__deals.sql
+++ src/test/resources/seeds/V4__deals.sql
@@ -1,37 +1,43 @@
-INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish) VALUES
-	 (50,'2022-07-27 13:15:04.809267','Discounted slices from 10:30 - 11:30.
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
+	 (64,'2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.
 
-                                       Verified two supreme slices for $7.09 (tax included) which is $1.20 discount per slice over menu price.','2022-09-03 07:50:36.052304',10000,NULL,'Pizza'),
-	 (58,'2022-07-27 23:16:36.873815','$1.10 crispy tacos (half price)','2022-09-03 07:50:55.368952',2,NULL,'Taco'),
-	 (65,'2022-07-27 23:16:57.658109','$1.00 off Taco Bravo','2022-09-03 07:51:11.750559',2,NULL,'Taco'),
-	 (64,'2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.','2022-09-03 07:51:27.245722',10000,NULL,'Pizza'),
-	 (68,'2022-08-10 17:24:21.696795','$1.39 softshell taco','2022-09-03 07:51:40.114854',2,NULL,'Taco'),
-	 (66,'2022-08-10 17:11:39.566796','$1.00 off Meat and Potato Burrito ($5.21 tax included)','2022-09-03 07:51:59.729095',2,NULL,'Burrito'),
-	 (67,'2022-08-10 17:23:55.716542','$1.00 off taco salad','2022-09-03 07:52:09.648306',2,NULL,'Taco'),
+Verified $7.09 (tax included) for two supreme slices on 09/13/2022.','2022-09-14 14:21:16.201131',10000,NULL,'Pizza',NULL,'admin',NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (58,'2022-07-27 23:16:36.873815','$1.10 crispy tacos (half price)','2022-09-03 07:50:55.368952',2,NULL,'Taco',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (65,'2022-07-27 23:16:57.658109','$1.00 off Taco Bravo','2022-09-03 07:51:11.750559',2,NULL,'Taco',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (68,'2022-08-10 17:24:21.696795','$1.39 softshell taco','2022-09-03 07:51:40.114854',2,NULL,'Taco',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (66,'2022-08-10 17:11:39.566796','$1.00 off Meat and Potato Burrito ($5.21 tax included)','2022-09-03 07:51:59.729095',2,NULL,'Burrito',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (67,'2022-08-10 17:23:55.716542','$1.00 off taco salad','2022-09-03 07:52:09.648306',2,NULL,'Taco',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
 	 (53,'2022-07-27 23:42:17.849534','$8.95 cajun cheeseburger ($2.30 off).
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:52:38.375949',10004,NULL,'Burger'),
+Discount does not appear to be applied with online ordering.','2022-09-03 07:52:38.375949',10004,NULL,'Burger',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
 	 (52,'2022-07-27 23:41:12.97514','$8.55 cheeseburger ($2.30 off).
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:52:46.216234',10004,NULL,'Burger');
-INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish) VALUES
+Discount does not appear to be applied with online ordering.','2022-09-03 07:52:46.216234',10004,NULL,'Burger',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
 	 (51,'2022-07-27 23:40:06.41481','$8.25 plain burger ($2.30 off).
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:52:55.723751',10004,NULL,'Burger'),
+Discount does not appear to be applied with online ordering.','2022-09-03 07:52:55.723751',10004,NULL,'Burger',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
 	 (54,'2022-07-27 23:45:21.760957','$10.25 bacon cheeseburger ($2.30 off).
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:53:04.933112',10004,NULL,'Burger'),
+Discount does not appear to be applied with online ordering.','2022-09-03 07:53:04.933112',10004,NULL,'Burger',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false);
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
 	 (55,'2022-07-27 23:46:01.33711','$9.95 mushroom swiss burger ($2.30 off).
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:53:10.855782',10004,NULL,'Burger'),
+Discount does not appear to be applied with online ordering.','2022-09-03 07:53:10.855782',10004,NULL,'Burger',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
 	 (56,'2022-07-27 23:46:38.15725','$9.95 Minnesota burger ($2.30 off).
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:53:16.817068',10004,NULL,'Burger'),
-	 (57,'2022-07-27 23:56:35.586779','Buy one hot dog get another half off','2022-09-03 07:53:25.306513',10011,NULL,'Hot Dog'),
-	 (59,'2022-07-27 23:59:01.748955','$4.50 for two Ball Park hot dogs','2022-09-03 07:53:29.92064',10011,NULL,'Hot Dog'),
-	 (60,'2022-07-27 23:59:20.578794','$5.50 for two Ball Park chili dogs','2022-09-03 07:53:39.386668',10011,NULL,'Hot Dog'),
+Discount does not appear to be applied with online ordering.','2022-09-03 07:53:16.817068',10004,NULL,'Burger',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (57,'2022-07-27 23:56:35.586779','Buy one hot dog get another half off','2022-09-03 07:53:25.306513',10011,NULL,'Hot Dog',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (59,'2022-07-27 23:59:01.748955','$4.50 for two Ball Park hot dogs','2022-09-03 07:53:29.92064',10011,NULL,'Hot Dog',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (60,'2022-07-27 23:59:20.578794','$5.50 for two Ball Park chili dogs','2022-09-03 07:53:39.386668',10011,NULL,'Hot Dog',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (63,'2022-07-28 00:03:39.268623','$3.00 (tax included) for any flavor small malt or root beer float.','2022-09-03 07:53:49.367803',10011,NULL,'Beverage',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
 	 (61,'2022-07-28 00:00:17.810187','$4.95 for a Vienna Beef hot dog with chips and a 24 ounce fountain drink.
-$2.75 to add a beef hot dog.','2022-09-03 07:53:44.621828',10011,NULL,'Hot Dog'),
-	 (63,'2022-07-28 00:03:39.268623','$3.00 (tax included) for any flavor small malt or root beer float.','2022-09-03 07:53:49.367803',10011,NULL,'Beverage'),
+$2.75 to add a beef hot dog.','2022-09-03 07:53:44.621828',10011,NULL,'Hot Dog',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
 	 (62,'2022-07-28 00:01:23.103103','$4.55 for a Ball Park hot dog with chips and a 24 ounce fountain drink.
-$2.25 to add a dog.','2022-09-03 07:53:54.866891',10011,NULL,'Hot Dog');
+$2.25 to add a dog.','2022-09-03 07:53:54.866891',10011,NULL,'Hot Dog',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,false,false),
+	 (50,'2022-07-27 13:15:04.809267','$3.20 slices from 10:30 - 11:30.
+
+Verified two supreme slices for $7.09 (tax included) which is $1.20 discount per slice over menu price.','2022-09-14 14:19:31.208196',10000,NULL,'Pizza',NULL,'admin',1.25,28.08,3.2,0.5,15.63,3.2,false,true),
+	 (108,'2022-09-14 14:22:38.712614','Free breadsticks with purchase of a slice.','2022-09-14 14:23:28.272776',10000,NULL,'Bread','admin','admin',1.6,NULL,4.45,NULL,NULL,3.7,false,true);
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
+	 (109,'2022-09-14 14:24:09.302423','12 chicken wings for $7.25','2022-09-14 14:25:37.909266',10000,NULL,'Chicken Wings','admin','admin',4.75,39.58,7.25,4.75,39.58,7.25,false,true),
+	 (110,'2022-09-14 14:30:16.881066','2 for 1 garlic cheese bread','2022-09-14 14:30:52.44915',10000,NULL,'Bread','admin','admin',2.4,50.0,2.4,2.4,50.0,2.4,false,true);
