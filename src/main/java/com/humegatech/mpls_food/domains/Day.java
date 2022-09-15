2f25dcc3d9cb2dcd4117f685496754c39d1b6049
eebbesen <eebbesen.git@gmail.com>
2022-09-15 08:43:53 -0500

    explicit fk constraint names

--- src/main/java/com/humegatech/mpls_food/domains/Day.java
+++ src/main/java/com/humegatech/mpls_food/domains/Day.java
@@ -7,10 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
@@ -24,7 +21,7 @@ import java.time.LocalDate;
 public class Day extends BaseEntity {
     @ManyToOne
     @JsonBackReference
-    @JoinColumn(name = "deal_id")
+    @JoinColumn(name = "deal_id", foreignKey = @ForeignKey(name = "fk_days_deals"))
     private Deal deal;
     private DayOfWeek dayOfWeek;
     private LocalDate date;
