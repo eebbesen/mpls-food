bb01c425ca670a176fea0524263893e254c14303
eebbesen <eebbesen.git@gmail.com>
2022-09-25 15:05:19 -0500

    updating data

--- src/test/test_data/deals_202209030755.sql
+++ src/test/test_data/deals_202209030755.sql
@@ -1,35 +1,39 @@
-INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish) VALUES
-	 (50,'2022-07-27 13:15:04.809267','$2.50 slices from 10:30 - 11:30','2022-09-03 07:50:36.052304',10000,NULL,'Pizza'),
-	 (58,'2022-07-27 23:16:36.873815','$1.10 crispy tacos (half price)','2022-09-03 07:50:55.368952',2,NULL,'Taco'),
-	 (65,'2022-07-27 23:16:57.658109','$1.00 off Taco Bravo','2022-09-03 07:51:11.750559',2,NULL,'Taco'),
-	 (64,'2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.','2022-09-03 07:51:27.245722',10000,NULL,'Pizza'),
-	 (68,'2022-08-10 17:24:21.696795','$1.39 softshell taco','2022-09-03 07:51:40.114854',2,NULL,'Taco'),
-	 (66,'2022-08-10 17:11:39.566796','$1.00 off Meat and Potato Burrito ($5.21 tax included)','2022-09-03 07:51:59.729095',2,NULL,'Burrito'),
-	 (67,'2022-08-10 17:23:55.716542','$1.00 off taco salad','2022-09-03 07:52:09.648306',2,NULL,'Taco'),
-	 (53,'2022-07-27 23:42:17.849534','$8.95 cajun cheeseburger ($2.30 off).
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
+	 (55,'2022-07-27 23:46:01.33711','$9.95 mushroom swiss burger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:52:38.375949',10004,NULL,'Burger'),
-	 (52,'2022-07-27 23:41:12.97514','$8.55 cheeseburger ($2.30 off).
+Discount does not appear to be applied with online ordering.','2022-09-15 05:25:47.141901',10004,'American','Burger',NULL,'admin',2.3,18.33,9.95,2.3,18.33,9.95,false,false),
+	 (56,'2022-07-27 23:46:38.15725','$9.95 Minnesota burger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:52:46.216234',10004,NULL,'Burger');
-INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish) VALUES
-	 (51,'2022-07-27 23:40:06.41481','$8.25 plain burger ($2.30 off).
+Discount does not appear to be applied with online ordering.','2022-09-15 05:28:10.280399',10004,'American','Burger',NULL,'admin',0.3,2.93,9.95,0.3,2.93,9.95,false,false),
+	 (66,'2022-08-10 17:11:39.566796','$1.00 off Meat and Potato Burrito ($5.21 tax included)','2022-09-14 21:34:11.497607',2,'Tex Mex','Burrito',NULL,'admin',1.0,NULL,5.21,1.0,NULL,5.21,true,true),
+	 (65,'2022-07-27 23:16:57.658109','$1.00 off Taco Bravo','2022-09-14 21:34:51.822252',2,'Tex Mex','Taco',NULL,'admin',1.0,NULL,NULL,1.0,NULL,NULL,false,true),
+	 (57,'2022-07-27 23:56:35.586779','Buy one hot dog get another half off','2022-09-14 21:35:17.477544',10011,'American','Hot Dog',NULL,'admin',NULL,25.0,5.18,NULL,25.0,3.36,false,true),
+	 (64,'2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.','2022-09-15 05:34:39.362197',10000,'Italian','Pizza',NULL,'admin',NULL,NULL,5.0,NULL,NULL,5.0,false,false),
+	 (67,'2022-08-10 17:23:55.716542','$1.00 off taco salad','2022-09-14 16:07:42.187979',2,'Tex Mex','Taco',NULL,'admin',1.0,NULL,NULL,1.0,NULL,NULL,false,true),
+	 (63,'2022-07-28 00:03:39.268623','$3.00 (tax included) for any flavor small malt or root beer float.','2022-09-15 05:35:12.126051',10011,'American','Beverage',NULL,'admin',NULL,NULL,3.0,NULL,NULL,3.0,true,false),
+	 (51,'2022-07-27 23:40:06.41481','$8.25 plain burger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:52:55.723751',10004,NULL,'Burger'),
-	 (54,'2022-07-27 23:45:21.760957','$10.25 bacon cheeseburger ($2.30 off).
+Discount does not appear to be applied with online ordering.','2022-09-15 05:35:44.269351',10004,'American','Burger',NULL,'admin',2.3,21.8,8.25,2.3,21.8,8.25,false,false),
+	 (52,'2022-07-27 23:41:12.97514','$8.55 cheeseburger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:53:04.933112',10004,NULL,'Burger'),
-	 (55,'2022-07-27 23:46:01.33711','$9.95 mushroom swiss burger ($2.30 off).
+Discount does not appear to be applied with online ordering.','2022-09-15 05:36:00.061396',10004,'American','Burger',NULL,'admin',2.3,21.2,8.55,2.3,21.2,8.55,false,false);
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
+	 (53,'2022-07-27 23:42:17.849534','$8.95 cajun cheeseburger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:53:10.855782',10004,NULL,'Burger'),
-	 (56,'2022-07-27 23:46:38.15725','$9.95 Minnesota burger ($2.30 off).
+Discount does not appear to be applied with online ordering.','2022-09-15 05:36:33.325159',10004,'American','Burger',NULL,'admin',2.3,20.44,8.95,2.3,20.44,8.95,false,false),
+	 (54,'2022-07-27 23:45:21.760957','$10.25 bacon cheeseburger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-03 07:53:16.817068',10004,NULL,'Burger'),
-	 (57,'2022-07-27 23:56:35.586779','Buy one hot dog get another half off','2022-09-03 07:53:25.306513',10011,NULL,'Hot Dog'),
-	 (59,'2022-07-27 23:59:01.748955','$4.50 for two Ball Park hot dogs','2022-09-03 07:53:29.92064',10011,NULL,'Hot Dog'),
-	 (60,'2022-07-27 23:59:20.578794','$5.50 for two Ball Park chili dogs','2022-09-03 07:53:39.386668',10011,NULL,'Hot Dog'),
-	 (61,'2022-07-28 00:00:17.810187','$4.95 for a Vienna Beef hot dog with chips and a 24 ounce fountain drink.
-$2.75 to add a beef hot dog.','2022-09-03 07:53:44.621828',10011,NULL,'Hot Dog'),
-	 (63,'2022-07-28 00:03:39.268623','$3.00 (tax included) for any flavor small malt or root beer float.','2022-09-03 07:53:49.367803',10011,NULL,'Beverage'),
+Discount does not appear to be applied with online ordering.','2022-09-15 05:36:58.115556',10004,'American','Burger',NULL,'admin',2.3,18.33,10.25,2.3,18.33,10.25,false,false),
+	 (58,'2022-07-27 23:16:36.873815','Half-price crispy tacos (Taco Tuesday). $1.19 pre-tax, $1.32 tax included.','2022-09-20 13:27:39.793612',2,'Tex Mex','Taco',NULL,'admin',1.19,50.0,1.19,1.19,50.0,1.19,false,true),
+	 (68,'2022-08-10 17:24:21.696795','$1.39 softshell taco','2022-09-25 14:48:44.871546',2,'Tex Mex','Taco',NULL,'admin',NULL,NULL,1.39,NULL,NULL,1.39,false,true),
+	 (60,'2022-07-27 23:59:20.578794','Two Ball Park chili dogs for $5.50','2022-09-25 14:51:33.341593',10011,'American','Hot Dog',NULL,'admin',NULL,NULL,5.5,NULL,NULL,5.5,false,true),
+	 (59,'2022-07-27 23:59:01.748955','Two Ball Park hot dogs for $4.50','2022-09-25 15:03:53.226737',10011,'American','Hot Dog',NULL,'admin',NULL,NULL,4.5,NULL,NULL,4.5,false,true),
 	 (62,'2022-07-28 00:01:23.103103','$4.55 for a Ball Park hot dog with chips and a 24 ounce fountain drink.
-$2.25 to add a dog.','2022-09-03 07:53:54.866891',10011,NULL,'Hot Dog');
+$2.25 to add a dog.','2022-09-14 21:22:24.631882',10011,'American','Hot Dog',NULL,'admin',NULL,NULL,6.8,NULL,NULL,4.55,false,true),
+	 (110,'2022-09-14 14:30:16.881066','2 for 1 garlic cheese bread','2022-09-14 21:34:39.290629',10000,'Italian','Bread','admin','admin',2.4,50.0,2.4,2.4,50.0,2.4,false,true),
+	 (109,'2022-09-14 14:24:09.302423','12 chicken wings for $7.25','2022-09-14 21:36:11.759125',10000,'American','Chicken Wings','admin','admin',4.75,39.58,7.25,4.75,39.58,7.25,false,true),
+	 (50,'2022-07-27 13:15:04.809267','$3.20 slices from 10:30 - 11:30.','2022-09-14 21:50:47.654027',10000,'Italian','Pizza',NULL,'admin',1.25,28.08,3.2,0.5,15.63,3.2,false,true);
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
+	 (108,'2022-09-14 14:22:38.712614','Free breadsticks with purchase of a slice.','2022-09-15 05:29:58.370784',10000,'Italian','Bread','admin','admin',1.6,30.19,4.45,1.6,26.45,3.7,false,true),
+	 (61,'2022-07-28 00:00:17.810187','$4.95 for a Vienna Beef hot dog with chips and a 24 ounce fountain drink.
+$2.75 to add a beef hot dog.','2022-09-15 05:33:49.064028',10011,'American','Hot Dog',NULL,'admin',NULL,NULL,7.7,NULL,NULL,4.95,false,false);
