b6a78cb9bd7801ce5da4fa3674da921cefeeeb72
eebbesen <eebbesen.git@gmail.com>
2022-09-25 15:43:58 -0500

    updating friday ginnelis deal

--- src/test/test_data/deals_202209030755.sql
+++ src/test/test_data/deals_202209030755.sql
@@ -8,7 +8,6 @@ Discount does not appear to be applied with online ordering.','2022-09-15 05:28:
 	 (66,'2022-08-10 17:11:39.566796','$1.00 off Meat and Potato Burrito ($5.21 tax included)','2022-09-14 21:34:11.497607',2,'Tex Mex','Burrito',NULL,'admin',1.0,NULL,5.21,1.0,NULL,5.21,true,true),
 	 (65,'2022-07-27 23:16:57.658109','$1.00 off Taco Bravo','2022-09-14 21:34:51.822252',2,'Tex Mex','Taco',NULL,'admin',1.0,NULL,NULL,1.0,NULL,NULL,false,true),
 	 (57,'2022-07-27 23:56:35.586779','Buy one hot dog get another half off','2022-09-14 21:35:17.477544',10011,'American','Hot Dog',NULL,'admin',NULL,25.0,5.18,NULL,25.0,3.36,false,true),
-	 (64,'2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.','2022-09-15 05:34:39.362197',10000,'Italian','Pizza',NULL,'admin',NULL,NULL,5.0,NULL,NULL,5.0,false,false),
 	 (67,'2022-08-10 17:23:55.716542','$1.00 off taco salad','2022-09-14 16:07:42.187979',2,'Tex Mex','Taco',NULL,'admin',1.0,NULL,NULL,1.0,NULL,NULL,false,true),
 	 (63,'2022-07-28 00:03:39.268623','$3.00 (tax included) for any flavor small malt or root beer float.','2022-09-15 05:35:12.126051',10011,'American','Beverage',NULL,'admin',NULL,NULL,3.0,NULL,NULL,3.0,true,false),
 	 (51,'2022-07-27 23:40:06.41481','$8.25 plain burger and fries.
@@ -16,11 +15,11 @@ Discount does not appear to be applied with online ordering.','2022-09-15 05:28:
 Discount does not appear to be applied with online ordering.','2022-09-15 05:35:44.269351',10004,'American','Burger',NULL,'admin',2.3,21.8,8.25,2.3,21.8,8.25,false,false),
 	 (52,'2022-07-27 23:41:12.97514','$8.55 cheeseburger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-15 05:36:00.061396',10004,'American','Burger',NULL,'admin',2.3,21.2,8.55,2.3,21.2,8.55,false,false);
-INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
+Discount does not appear to be applied with online ordering.','2022-09-15 05:36:00.061396',10004,'American','Burger',NULL,'admin',2.3,21.2,8.55,2.3,21.2,8.55,false,false),
 	 (53,'2022-07-27 23:42:17.849534','$8.95 cajun cheeseburger and fries.
 
-Discount does not appear to be applied with online ordering.','2022-09-15 05:36:33.325159',10004,'American','Burger',NULL,'admin',2.3,20.44,8.95,2.3,20.44,8.95,false,false),
+Discount does not appear to be applied with online ordering.','2022-09-15 05:36:33.325159',10004,'American','Burger',NULL,'admin',2.3,20.44,8.95,2.3,20.44,8.95,false,false);
+INSERT INTO public.deals (id,date_created,description,last_updated,place_id,cuisine,dish,created_by,modified_by,max_discount,max_discount_percent,max_price,min_discount,min_discount_percent,min_price,tax_included,verified) VALUES
 	 (54,'2022-07-27 23:45:21.760957','$10.25 bacon cheeseburger and fries.
 
 Discount does not appear to be applied with online ordering.','2022-09-15 05:36:58.115556',10004,'American','Burger',NULL,'admin',2.3,18.33,10.25,2.3,18.33,10.25,false,false),
@@ -28,6 +27,7 @@ Discount does not appear to be applied with online ordering.','2022-09-15 05:36:
 	 (68,'2022-08-10 17:24:21.696795','$1.39 softshell taco','2022-09-25 14:48:44.871546',2,'Tex Mex','Taco',NULL,'admin',NULL,NULL,1.39,NULL,NULL,1.39,false,true),
 	 (60,'2022-07-27 23:59:20.578794','Two Ball Park chili dogs for $5.50','2022-09-25 14:51:33.341593',10011,'American','Hot Dog',NULL,'admin',NULL,NULL,5.5,NULL,NULL,5.5,false,true),
 	 (59,'2022-07-27 23:59:01.748955','Two Ball Park hot dogs for $4.50','2022-09-25 15:03:53.226737',10011,'American','Hot Dog',NULL,'admin',NULL,NULL,4.5,NULL,NULL,4.5,false,true),
+	 (64,'2022-07-28 00:14:24.973642','$5.00 for two slices from 10:30 - 11:00.','2022-09-25 15:40:57.541839',10000,'Italian','Pizza',NULL,'admin',3.9,43.82,5.0,2.4,32.43,5.0,false,false),
 	 (62,'2022-07-28 00:01:23.103103','$4.55 for a Ball Park hot dog with chips and a 24 ounce fountain drink.
 $2.25 to add a dog.','2022-09-14 21:22:24.631882',10011,'American','Hot Dog',NULL,'admin',NULL,NULL,6.8,NULL,NULL,4.55,false,true),
 	 (110,'2022-09-14 14:30:16.881066','2 for 1 garlic cheese bread','2022-09-14 21:34:39.290629',10000,'Italian','Bread','admin','admin',2.4,50.0,2.4,2.4,50.0,2.4,false,true),
