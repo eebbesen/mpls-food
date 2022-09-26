7773f9dff9bdab99d141507978e4000c216c509c
eebbesen <eebbesen.git@gmail.com>
2022-09-26 15:27:21 -0500

    adding id to auth table inserts

--- src/test/test_data/users_202208090833.sql
+++ src/test/test_data/users_202208090833.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.users (username,"password",enabled) VALUES
-	 ('user','$2a$04$vfJTzvsrzJc1AsOdgjb3ge3TX1ZUTeAJa3TIS8Yzqen4t6NMx7xvm',true),
-	 ('admin','$2a$04$vfJTzvsrzJc1AsOdgjb3ge3TX1ZUTeAJa3TIS8Yzqen4t6NMx7xvm',true);
+INSERT INTO public.users (id,username,"password",enabled) VALUES
+	 (1,'user','$2a$04$vfJTzvsrzJc1AsOdgjb3ge3TX1ZUTeAJa3TIS8Yzqen4t6NMx7xvm',true),
+	 (2,'admin','$2a$04$vfJTzvsrzJc1AsOdgjb3ge3TX1ZUTeAJa3TIS8Yzqen4t6NMx7xvm',true);
