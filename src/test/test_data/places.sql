3df70d29d9990f601ecf3dbd16c2cf9b9e9faa3f
eebbesen <eebbesen.git@gmail.com>
2022-10-16 08:19:01 -0500

    more data

--- src/test/test_data/places.sql
+++ src/test/test_data/places.sql
@@ -33,9 +33,39 @@ Suite 230
 Minneapolis, MN 55415',true,'2022-10-09 14:31:00.441405','2022-10-09 14:31:00.441405','Subway 4th Ave',true,'https://restaurants.subway.com/united-states/mn/minneapolis/701-4th-avenue-south','admin','admin'),
 	 (26,'200 South 5th Street
 Minneapolis, MN 55402',true,'2022-10-09 14:31:45.36618','2022-10-09 14:31:45.36618','Subway 5th St',true,'https://restaurants.subway.com/united-states/mn/minneapolis/200-south-5th-street','admin','admin'),
+	 (44,'720 3rd Street South
+Minneapolis, MN 55415',true,'2022-10-13 10:56:29.489237','2022-10-13 10:56:29.489237','Jimmy John''s #3836',true,'https://locations.jimmyjohns.com/mn/minneapolis/sandwiches-3836.html','admin','admin'),
 	 (33,'527 S Marquette Ave
 Minneapolis, MN 55402',true,'2022-10-12 23:00:59.643811','2022-10-12 23:03:17.04248','Potbelly Rand Tower',true,'https://www.potbelly.com/perks','admin','admin'),
 	 (22,'555 Nicollet Mall Suite 297
-Minneapolis, MN 55402',true,'2022-10-09 14:27:13.95516','2022-10-12 23:03:25.87861','Subway Nicollet',true,'https://restaurants.subway.com/united-states/mn/minneapolis/555-nicollet-mall','admin','admin'),
+Minneapolis, MN 55402',true,'2022-10-09 14:27:13.95516','2022-10-12 23:03:25.87861','Subway Nicollet',true,'https://restaurants.subway.com/united-states/mn/minneapolis/555-nicollet-mall','admin','admin');
+INSERT INTO public.places (id,address,app,date_created,last_updated,"name",order_ahead,website,created_by,modified_by) VALUES
+	 (40,'50 South 6th St Ste 240
+Minneapolis, MN 55402',true,'2022-10-13 08:43:39.459999','2022-10-13 10:59:13.948588','Chipotle 50 South 6th',true,'https://locations.chipotle.com/mn/minneapolis/50-s-6th-st','admin','admin'),
 	 (28,'931 Nicollet Mall
-Minneapolis, MN 55402',false,'2022-10-09 14:38:56.14607','2022-10-12 23:07:30.687316','The Local',true,'https://the-local.com/pub-club/','admin','admin');
+Minneapolis, MN 55402',false,'2022-10-09 14:38:56.14607','2022-10-12 23:07:30.687316','The Local',true,'https://the-local.com/pub-club/','admin','admin'),
+	 (45,'200 South 6th St
+Minneapolis, MN 55402',true,'2022-10-13 11:00:39.772978','2022-10-13 11:00:39.772978','Chipotle 200 South 6th',true,'https://chipotle.com/','admin','admin'),
+	 (35,'200 South 6th St Ste 280
+Minneapolis, MN 55402',false,'2022-10-12 23:51:31.57337','2022-10-12 23:59:11.282884','Jr. Brothers',true,'https://www.jrbrothersthai.com/order#signup','admin','admin'),
+	 (36,'200 South 6th St
+Minneapolis, MN 55402',true,'2022-10-13 07:41:07.693369','2022-10-13 07:41:07.693369','Naf Naf US Bank Plaza',true,'https://www.nafnafgrill.com/perks/','admin','admin'),
+	 (37,'40 S 7th St #206
+Minneapolis, MN 55402',true,'2022-10-13 07:42:38.42555','2022-10-13 07:42:38.42555','Naf Naf City Center',true,'https://www.nafnafgrill.com/perks/','admin','admin'),
+	 (38,'651 Nicollet Mall
+Minneapolis, MN 55402',true,'2022-10-13 08:03:17.723368','2022-10-13 08:03:17.723368','Freshii',true,'https://www.freshii.com/us/en-us/rewards','admin','admin'),
+	 (46,'1040 Nicollet Mall
+Minneapolis, MN 55403',true,'2022-10-13 11:01:14.102068','2022-10-13 11:01:14.102068','Chipotle Nicollet',true,'https://chipotle.com','admin','admin'),
+	 (39,'40 S 7th St
+Minneapolis, MN 55402',true,'2022-10-13 08:36:19.305232','2022-10-13 08:45:45.106076','Leeann Chin',true,'https://www.leeannchin.com/restaurant/city-centerdowntown-minneapolis-mn/40-south-7th-street','admin','admin'),
+	 (41,'600 Hennepin Ave S, Ste 140
+Minneapolis, MN 55403',true,'2022-10-13 10:52:25.956979','2022-10-13 10:52:25.956979','Jimmy John''s #191',true,'https://locations.jimmyjohns.com/mn/minneapolis/sandwiches-191.html','admin','admin');
+INSERT INTO public.places (id,address,app,date_created,last_updated,"name",order_ahead,website,created_by,modified_by) VALUES
+	 (42,'Pillsbury Center, 200 South 6th St
+Minneapolis, MN 55402',true,'2022-10-13 10:53:38.577923','2022-10-13 10:53:38.577923','Jimmy John''s #192 Pillsbury',true,'https://locations.jimmyjohns.com/mn/minneapolis/sandwiches-192.html','admin','admin'),
+	 (43,'88 S 9th St, Minneapolis
+MN 55402',true,'2022-10-13 10:55:04.962617','2022-10-13 10:55:04.962617','Jimmy John''s #190',true,'https://locations.jimmyjohns.com/mn/minneapolis/sandwiches-190.html','admin','admin'),
+	 (47,'50 South 10th St, Nicollet Mall
+Minneapolis, MN 55403',true,'2022-10-13 11:18:58.565185','2022-10-13 11:18:58.565185','Qdoba',true,'https://locations.qdoba.com/us/mn/minneapolis/50-south-10th-st.html','admin','admin'),
+	 (48,'809 Nicollet Mall
+Minneapolis, MN 55402',true,'2022-10-13 11:33:56.752244','2022-10-13 11:33:56.752244','Panera Bread',true,'https://www.panerabread.com','admin','admin');
