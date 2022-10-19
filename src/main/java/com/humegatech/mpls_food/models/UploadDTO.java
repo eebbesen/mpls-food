cf418a2b63bd5e52ffb3683cf7fd28181b0b4b29
eebbesen <eebbesen.git@gmail.com>
2022-10-19 14:09:53 -0500

    removing unused fields from UploadDTO

--- src/main/java/com/humegatech/mpls_food/models/UploadDTO.java
+++ src/main/java/com/humegatech/mpls_food/models/UploadDTO.java
@@ -2,19 +2,13 @@ package com.humegatech.mpls_food.models;
 
 import lombok.*;
 
-import java.time.OffsetDateTime;
-
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UploadDTO {
-    private Long id;
     private Long dealId;
     private byte[] image;
     private boolean verified;
-    private String createdBy;
-    private OffsetDateTime dateCreated;
-    private String dealDescription;
 }
