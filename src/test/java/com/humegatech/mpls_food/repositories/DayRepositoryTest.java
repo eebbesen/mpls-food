aa8c60670125cc8c6023cabbc56b265306573f22
J2EEbbesen <eebbesen@users.noreply.github.com>
2023-01-11 10:14:07 -0600

    adding test coverage for one-day deals (#98)

--- src/test/java/com/humegatech/mpls_food/repositories/DayRepositoryTest.java
+++ src/test/java/com/humegatech/mpls_food/repositories/DayRepositoryTest.java
@@ -7,34 +7,37 @@ import com.humegatech.mpls_food.domains.Place;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@DataJpaTest
 class DayRepositoryTest {
     @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
     private DayRepository dayRepository;
-
     @Autowired
     private DealRepository dealRepository;
-
     @Autowired
     private PlaceRepository placeRepository;
-
     private Place place;
+
 
     @BeforeEach
     void setUp() {
+
         place = TestObjects.place("my place");
         place.setId(null);
-        place = placeRepository.save(place);
+        entityManager.persist(place);
     }
 
     private void buildAndSaveDay(final Deal deal) {
@@ -46,7 +49,7 @@ class DayRepositoryTest {
                 .build();
         deal.setDays(Set.of(day));
 
-        dealRepository.save(deal);
+        entityManager.persist(deal);
     }
 
     @Test
@@ -71,5 +74,28 @@ class DayRepositoryTest {
         assertEquals(2, days.size());
         assertEquals(noDates.getId(), days.get(0).getDeal().getId());
         assertEquals(endDateFuture.getId(), days.get(1).getDeal().getId());
+    }
+
+    @Test
+    void testFindAllActiveOneDayDealTodayOnly() {
+        final LocalDate today = LocalDate.now();
+        final Deal deal = TestObjects.deal();
+        deal.setDescription("Today only");
+        deal.setId(null);
+        deal.setPlace(place);
+        deal.setStartDate(today);
+        deal.setEndDate(today);
+        deal.setDays(new HashSet<Day>());
+        final Day day = Day.builder()
+                .dayOfWeek(today.getDayOfWeek())
+                .deal(deal)
+                .build();
+        deal.getDays().add(day);
+        entityManager.persist(deal);
+
+        final List<Day> days = dayRepository.findAllActive();
+
+        assertEquals(1, days.size());
+        assertEquals(deal.getDescription(), days.get(0).getDeal().getDescription());
     }
 }
