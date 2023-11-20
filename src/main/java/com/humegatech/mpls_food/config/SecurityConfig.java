70e7c02fc7d1ef64ca7951e4b8956067d06e349b
eebbesen <eebbesen.git@gmail.com>
2023-11-20 09:11:34 -0600

    Revert "Changing security config for Spring Boot 3"
    This reverts commit 8f4cab098a4eb226b82464a1aa74419c29b42d12.

--- src/main/java/com/humegatech/mpls_food/config/SecurityConfig.java
+++ src/main/java/com/humegatech/mpls_food/config/SecurityConfig.java
@@ -1,89 +1,48 @@
 package com.humegatech.mpls_food.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import javax.sql.DataSource;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
-public class SecurityConfig {
-    // https://github.com/spring-projects/spring-security-samples/blob/5.8.x/servlet/spring-boot/java/hello-security-explicit/src/main/java/example/SecurityConfiguration.java
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // https://docs.spring.io/spring-security/site/docs/5.3.9.RELEASE/reference/html5/#servlet-authorization
 
-    private static final String H2_URL = "/h2-console";
-    private static final String SPRING = "";
     @Autowired
     private DataSource dataSource;
 
-    //    @Autowired
-//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery("select username, password, enabled from users where username=?")
-//                .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
-//    }
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.jdbcAuthentication()
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
                 .dataSource(dataSource)
                 .usersByUsernameQuery("select username, password, enabled from users where username=?")
                 .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http,
-                                                   final HandlerMappingIntrospector introspector) throws Exception {
-        final MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
-        final MvcRequestMatcher h2 = filterPattern(mvcMatcherBuilder, "/**", H2_URL);
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.ignoringRequestMatchers(h2))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(h2).permitAll()
-                        .requestMatchers(filterPattern(mvcMatcherBuilder,"/css/**"),
-                                filterPattern(mvcMatcherBuilder,"/js/**"),
-                                filterPattern(mvcMatcherBuilder,"/images/**"),
-                                filterPattern(mvcMatcherBuilder,"/days"),
-                                filterPattern(mvcMatcherBuilder,"/deals"),
-                                filterPattern(mvcMatcherBuilder,"/deal_logs"),
-                                filterPattern(mvcMatcherBuilder,"/deal_logs/show/*"),
-                                filterPattern(mvcMatcherBuilder,"/login"),
-                                filterPattern(mvcMatcherBuilder,"/places"),
-                                filterPattern(mvcMatcherBuilder,"/places/show"),
-                                filterPattern(mvcMatcherBuilder,"/")).permitAll()
-                        .requestMatchers(filterPattern(mvcMatcherBuilder,"/*/add"),
-                                filterPattern(mvcMatcherBuilder,"/*/edit"),
-                                filterPattern(mvcMatcherBuilder,"/uploads/**")).authenticated()
-                        .requestMatchers(filterPattern(mvcMatcherBuilder,"/actuator"), // todo needed given next line?
-                                filterPattern(mvcMatcherBuilder,"/actuator/*"),
-                                filterPattern(mvcMatcherBuilder,"/deals/copy/"),
-                                filterPattern(mvcMatcherBuilder,"/deals/delete/*"),
-                                filterPattern(mvcMatcherBuilder,"/deal_logs/delete/*"),
-                                filterPattern(mvcMatcherBuilder,"/places/delete/*")).hasRole("ADMIN")
-                        .anyRequest().denyAll());
+                .authorizeRequests()
+                .mvcMatchers("/css/**", "/places", "/deals", "/days", "/deal_logs", "/login", "/", "/js/**",
+                        "/images/**", "/places/show/*", "/deal_logs/show/*").permitAll()
+                .mvcMatchers("/deals/add", "/places/add", "/uploads/**", "/deals/edit/*", "/deal_logs/add",
+                        "/deal_logs/edit/*")
+                .authenticated()
+                .mvcMatchers("/places/delete/*", "/*/edit/*", "/deals/delete/*", "/deals/copy/*",
+                        "/days/delete/*", "/deal_logs/delete/*", "/actuator", "/actuator/*", "").hasRole("ADMIN")
 
-        return http.build();
+                .anyRequest().denyAll();
+        // http.httpBasic();
+        http.formLogin();
     }
 
     //    @Autowired
@@ -92,19 +51,5 @@ public class SecurityConfig {
     //            .inMemoryAuthentication()
     //            .withUser("user").password("password").roles("USER");
     //    }
-
-    static MvcRequestMatcher filterPattern(final MvcRequestMatcher.Builder builder, final String pattern, final String servletPath) {
-        MvcRequestMatcher matcher = builder.pattern(pattern);
-
-        if (null !=  servletPath) {
-            matcher.setServletPath(servletPath);
-        }
-
-        return matcher;
-    }
-
-    static MvcRequestMatcher filterPattern(final MvcRequestMatcher.Builder builder, final String pattern) {
-        return filterPattern(builder, pattern, null);
-    }
 
 }
